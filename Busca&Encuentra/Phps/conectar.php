071cbb1b76ac15a73fcb5d9426ba490288825f73
Ozzyy18 <95302172+Ozzyy18@users.noreply.github.com>
2022-09-09 10:14:46 -0500

    Add files via upload

---
+++ Busca&Encuentra/Phps/conectar.php
@@ -0,0 +1,8 @@
+<?php
+    $db = mysqli_connect('localhost','root','123456','sysbusca&encuentra');
+    if(!$db)
+    {
+        echo("Error de conexion");
+        exit;
+    }
+   echo ("Conexion correcta");
