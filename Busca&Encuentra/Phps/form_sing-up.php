98e6781deb12ba76d946671a688cae2956e6660f
Ozzyy18 <roberto.andrade5167@alumnos.udg.mx>
2022-09-27 10:20:09 -0500

    Cambiar seguridad

--- Busca&Encuentra/Phps/form_sing-up.php
+++ Busca&Encuentra/Phps/form_sing-up.php
@@ -1,29 +1,20 @@
 <?php
 require "conectar.php";
-$nombres=$_GET["nombre"];
-$apellido_pa=$_GET["apellido_Pa"];
-$apellido_ma=$_GET["apellido_Ma"];
-$correo_electronico=$_GET["correo_elec"];
-$usuario=$_GET["usuario"]; //usuario va para primarikey
-$contrasena=$_GET["contraseña"];
-$contrasena_confirma=$_GET["confirma_contra"];
+$nombres=$_POST["nombre"];
+$apellido_pa=$_POST["apellido_Pa"];
+$apellido_ma=$_POST["apellido_Ma"];
+$correo_electronico=$_POST["correo_elec"];
+$usuario=$_POST["usuario"]; //usuario va para primarikey
+$contrasena=$_POST["contraseña"];
+$contrasena_confirma=$_POST["confirma_contra"];
 
-if(Saber_usuario($usuario,$db)==1)
-{
-    echo 2;
-}
-else
-{
+
     $insert_sql="INSERT INTO usuarios (username,correo,nombre,apellido_paterno,apellido_materno,contrasena)  
     VALUES ('$usuario','$correo_electronico','$nombres','$apellido_pa','$apellido_ma','$contrasena')";
     $consulta=mysqli_query($db,$insert_sql);
 
-}
 
-
-
-
-/*if($consulta)
+if($consulta)
 {
     echo "correcto";
 }
@@ -31,21 +22,8 @@ else
 else
 {
     echo "incorrecto";
-}*/
-
-function Saber_usuario($usser,$conexion)
-{
-    $sql="SELECT username FROM usuarios WHERE username=$usser";
-    $resultado=mysqli_query($conexion,$sql);
-
-    if(mysqli_num_rows($resultado)>0)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
 }
+
+
 
 ?>
