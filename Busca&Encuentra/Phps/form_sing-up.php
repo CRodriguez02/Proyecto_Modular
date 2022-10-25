50f7fa1850e3333adc09e67a0e3fec1168be02f0
Ozzyy18 <roberto.andrade5167@alumnos.udg.mx>
2022-10-25 17:28:28 -0500

    YA hace la validacion

--- Busca&Encuentra/Phps/form_sing-up.php
+++ Busca&Encuentra/Phps/form_sing-up.php
@@ -1,29 +1,43 @@
 <?php
-require "conectar.php";
-$nombres=$_POST["nombre"];
-$apellido_pa=$_POST["apellido_Pa"];
-$apellido_ma=$_POST["apellido_Ma"];
-$correo_electronico=$_POST["correo_elec"];
-$usuario=$_POST["usuario"]; //usuario va para primarikey
-$contrasena=$_POST["contraseña"];
-$contrasena_confirma=$_POST["confirma_contra"];
+    error_reporting(0);
+    require "conectar.php";
+    $nombres=$_POST["nombre"];
+    $apellido_pa=$_POST["apellido_Pa"];
+    $apellido_ma=$_POST["apellido_Ma"];
+    $correo_electronico=$_POST["correo_elec"];
+    $usuario=$_POST["usuario"]; //usuario va para primarikey
+    $contrasena=$_POST["contraseña"];
+    $contrasena_confirma=$_POST["confirma_contra"];
 
 
-    //$checar_usuario="if(username==$usuario)FROM usuarios"
-
-        $insert_sql="INSERT INTO usuarios (username,correo,nombre,apellido_paterno,apellido_materno,contrasena)  
-        VALUES ('$usuario','$correo_electronico','$nombres','$apellido_pa','$apellido_ma','$contrasena')";
-        $consulta=mysqli_query($db,$insert_sql);
-
-    if($consulta)
+    //Consulta para verificar que el registro no exista
+    $validar = "SELECT * FROM usuarios where username='$usuario'";
+    $validando=$db->query($validar);
+    if($validando->num_rows>0)
     {
-        echo "correcto";
+        echo("el usuario esta repetido");
     }
-
     else
     {
-        echo "incorrecto";
+            //$checar_usuario="if(username==$usuario)FROM usuarios"
+
+            $insert_sql="INSERT INTO usuarios (username,correo,nombre,apellido_paterno,apellido_materno,contrasena)  
+            VALUES ('$usuario','$correo_electronico','$nombres','$apellido_pa','$apellido_ma','$contrasena')";
+            $consulta=mysqli_query($db,$insert_sql);
+
+        if($consulta)
+        {
+            echo "correcto si estamos en este proyecto";
+        }
+
+        else
+        {
+            echo "incorrecto";
+        }
+
     }
+
+        
 
 
 
--- Busca&Encuentra/Phps/funciones.php
+++ Busca&Encuentra/Phps/funciones.php
@@ -30,6 +30,5 @@ function ObtenerServicios()
     }
 }
 
-ObtenerServicios();
 
 ?>
