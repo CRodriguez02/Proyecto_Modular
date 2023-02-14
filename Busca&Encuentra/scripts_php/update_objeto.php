a3a296c50bfaa1cbd9e38f779529666554f2fd29
Ozzyy18 <roberto.andrade5167@alumnos.udg.mx>
2023-02-14 15:56:07 -0600

    tac

--- Busca&Encuentra/scripts_php/update_objeto.php
+++ Busca&Encuentra/scripts_php/update_objeto.php
@@ -1,7 +1,29 @@
 <?php
 //lo que te manden de edit-publicacion aqui vas a usar una sentencia sql para hacer el update
 //vas a actualizar la imagen,titulo,descripcion,el tipo, practicamente copiar y el de subir objeto
-session_start()
+//error_reporting(0);
+require("conectar.php");
+session_start();
+$_SESSION['usuario'];
+//7 campos||   username,correo,nombre,apellido_patern,apellido_materno,contrasena,imagen;
+
+$username=$_SESSION['usuario'];//llave forane
+//ID
+$titulo=$_POST["txt_titulo"];
+$descripcion=$_POST["txt_descripcion"];
+$imagen=addslashes(file_get_contents($_FILES["imagen"]["tmp_name"]));
+$estado=$_POST["motivo"];//OBJETO ENCONTRADO O NO
+$tipo_objeto=$_POST["tipo_objeto"];
+$recompensa=$_POST["recompensa"];
+
+$sentencia_sql="INSERT INTO objeto (fk_username_Objeto,titulo,descripcion,imagen,estado,categoria,recompensa)
+values('$username','$titulo','$descripcion','$imagen',$estado,'$tipo_objeto',$recompensa);";
+$valida_consulta=$db->query($sentencia_sql);
+if($valida_consulta)
+{
+   header("Location: ../publicacion.php");
+}
+
 
 
 
