d8a6a4f0a03ced754c6ddb688210e86cd62fdb4f
CRodriguez02 <81532402+CRodriguez02@users.noreply.github.com>
2023-02-23 15:12:50 -0600

    cambio estetico

--- Busca&Encuentra/system-chat.php
+++ Busca&Encuentra/system-chat.php
@@ -157,7 +157,7 @@
                   <li class="p-2 border-bottom" style="background-color: #eee;">
                     <a href="#!" class="d-flex justify-content-between">
                       <div class="d-flex flex-row">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-8.webp" alt="avatar"
+                        <img src="assets/img/user-icon.png" alt="avatar"
                           class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
                         <div class="pt-1">
                           <p class="fw-bold mb-0">John Doe</p>
@@ -173,7 +173,7 @@
                   <li class="p-2 border-bottom">
                     <a href="#!" class="d-flex justify-content-between">
                       <div class="d-flex flex-row">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-1.webp" alt="avatar"
+                        <img src="assets/img/user-icon.png" alt="avatar"
                           class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
                         <div class="pt-1">
                           <p class="fw-bold mb-0">Danny Smith</p>
@@ -188,7 +188,7 @@
                   <li class="p-2 border-bottom">
                     <a href="#!" class="d-flex justify-content-between">
                       <div class="d-flex flex-row">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-2.webp" alt="avatar"
+                        <img src="assets/img/user-icon.png" alt="avatar"
                           class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
                         <div class="pt-1">
                           <p class="fw-bold mb-0">Alex Steward</p>
@@ -203,7 +203,7 @@
                   <li class="p-2 border-bottom">
                     <a href="#!" class="d-flex justify-content-between">
                       <div class="d-flex flex-row">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-3.webp" alt="avatar"
+                        <img src="assets/img/user-icon.png" alt="avatar"
                           class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
                         <div class="pt-1">
                           <p class="fw-bold mb-0">Ashley Olsen</p>
@@ -218,7 +218,7 @@
                   <li class="p-2 border-bottom">
                     <a href="#!" class="d-flex justify-content-between">
                       <div class="d-flex flex-row">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-4.webp" alt="avatar"
+                        <img src="assets/img/user-icon.png" alt="avatar"
                           class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
                         <div class="pt-1">
                           <p class="fw-bold mb-0">Kate Moss</p>
@@ -233,7 +233,7 @@
                   <li class="p-2 border-bottom">
                     <a href="#!" class="d-flex justify-content-between">
                       <div class="d-flex flex-row">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp" alt="avatar"
+                        <img src="assets/img/user-icon.png" alt="avatar"
                           class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
                         <div class="pt-1">
                           <p class="fw-bold mb-0">Lara Croft</p>
@@ -248,7 +248,7 @@
                   <li class="p-2">
                     <a href="#!" class="d-flex justify-content-between">
                       <div class="d-flex flex-row">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar"
+                        <img src="assets/img/user-icon.png" alt="avatar"
                           class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
                         <div class="pt-1">
                           <p class="fw-bold mb-0">Brad Pitt</p>
@@ -272,7 +272,7 @@
     
             <ul class="list-unstyled">
               <li class="d-flex justify-content-between mb-4">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar"
+                <img src="assets/img/user-icon.png" alt="avatar"
                   class="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60">
                 <div class="card">
                   <div class="card-header d-flex justify-content-between p-3">
@@ -300,11 +300,11 @@
                     </p>
                   </div>
                 </div>
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp" alt="avatar"
+                <img src="assets/img/user-icon.png" alt="avatar"
                   class="rounded-circle d-flex align-self-start ms-3 shadow-1-strong" width="60">
               </li>
               <li class="d-flex justify-content-between mb-4">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar"
+                <img src="assets/img/user-icon.png" alt="avatar"
                   class="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60">
                 <div class="card">
                   <div class="card-header d-flex justify-content-between p-3">
@@ -322,7 +322,7 @@
               <li class="bg-white mb-3">
                 <div class="form-outline">
                   <textarea class="form-control" id="textAreaExample2" rows="4"></textarea>
-                  <label class="form-label" for="textAreaExample2">Message</label>
+                  <label class="form-label" for="textAreaExample2">Mensaje</label>
                 </div>
               </li>
               <button type="button" class="btn btn-info btn-rounded float-end">Send</button>
